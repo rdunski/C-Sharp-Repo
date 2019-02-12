744156dc2e307b4a4aaa1a0b4c60b561569175c9
Robert Dunski <rdunski411@gmail.com>
2019-02-11 23:11:36 -0700

    Finished for now

--- Advanced Programming/Project3.cs
+++ Advanced Programming/Project3.cs
@@ -35,32 +35,50 @@ namespace CalculatorApp
     {
         static void Main(string[] args)
         {
-            Function f = null;
+            Function calc = null;
             double answer = 0.0;
+            string[] numbers = null;
+            string[] operators = null;
 
             Console.WriteLine(
                 "Welcome to the C# calculator app!\n" +
-                "Please enter your calculation in the form\n" +
-                "number operator number");
-            string[] numbers;
-            string[] operators;
+                "Please enter your calculation in the form:\n" +
+                "number operator number   Ex. 1*2/3+4-5\n" +
+                "Type 'exit' anytime to close the calculator\n");
+
             while (true)
             {
-                // Read user request for calculation
+                int counter = 0; //Counter to iterate through expression, needs to be reset after every expression
                 string request = Console.ReadLine();
-                // Parse requested calculation
-                numbers = request.Split("/+-*".ToCharArray());
-                operators = request.Split("1234567890".ToCharArray());
-                foreach(string op in operators)
+                if (request == "exit")
+                  break;
+
+                numbers = request.Split("/+-* ".ToCharArray()); //Remove operators to get numbers
+                operators = request.Split("1234567890".ToCharArray()); //Remove numbers to get operators
+                answer = Convert.ToDouble(numbers[counter]);  //Start with first number in expression
+                foreach(string op in operators) //For every operator in the expression, allowing for multiple operations in one expression
                 {
-                  if (op == "+")
-                  {
-                    Function add = Functions.Add;
-                    answer = add((double)numbers[Array.IndexOf(operators,"+")-1], (double)numbers[Array.IndexOf(operators, "+")]);
-                  }
+                  if (op == "*")            //Emulating PEMDAS (without the parentheses/exponents for now)
+                    calc = Functions.Mult;
+                  else if (op == "/")
+                    calc = Functions.Div;
+                  else if (op == "+")
+                    calc = Functions.Add;
+                  else if (op == "-")
+                    calc = Functions.Sub;
+                  else                      //If no operator found, continue until one is found or it's the end of expression
+                    continue;
+
+                  answer = calc(answer,Convert.ToDouble(numbers[counter+1])); //The heavy lifting
+
+                  counter++;
+
+                  continue;                 //Keep finding operators
                 }
+
+                Console.WriteLine("The answer is: {0}", answer);
                 // Assign the function requested to your delegate
-                break;
+
                 // Invoke the delegate function to get the answer
 
                 // Display answer
@@ -68,7 +86,7 @@ namespace CalculatorApp
             }
 
             Console.WriteLine("Press any key to continue");
-            Console.Write("{0} {1}", answer, operators[2]);
+
             Console.ReadKey();
         }
     }

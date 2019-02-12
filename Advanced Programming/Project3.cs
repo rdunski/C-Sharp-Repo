ed1916134f87fc2e0145f301bf612bc2cc5d96f9
Robert J Dunski <rdunski411@gmail.com>
2019-02-11 23:35:07 -0700

    Update Project3.cs

--- Advanced Programming/Project3.cs
+++ Advanced Programming/Project3.cs
@@ -7,26 +7,15 @@ using CalcLib;
 
 /// Calculator app project.
 ///
-/// This is a two-part project,
-/// One part is a console app user interface
-/// allowing the user to enter operands (values) and operators.
-/// The console app will perform the requested operation
-/// and display the results.
-/// (The user should be able to do multiple calculations
-/// without restarting the app.)
-///
-/// The other part is a class library assembly
-/// exposing static methods that implement the operations
-/// needed to support the calculator user interface.
-/// (This will require two projects in your solution,
-/// and you will have two .cs.txt files to turn in.)
-///
-/// The calculator user interface console app will use a delegate
-/// to refer to the various operations in the class library.
-///
-/// In the console app project, you will need to include
-/// a reference to the class library and you will need to
-/// include a using namespace.
+// Made by: Robert Dunski, 02/11/2019
+//
+// Project 3 for Advanced Programming: C#
+//
+// This console app takes several user-entered expressions and performs necessary operations
+// on them to find the answer. Input is either decimals or integers, and +, -, *, and / can be used
+// as valid operators. Long expressions are also possible, such as 1*2*3/4/5, and as far as I've tested, 
+// the calculator also determines PEMDAS, but with only 4 operators of course. Negatives are currently
+// not valid for input, but can be generated in the output
 namespace CalculatorApp
 {
     delegate double Function(double x, double y);
